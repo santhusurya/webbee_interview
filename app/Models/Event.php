1d3adb7629466f630ef761b2d91f16a7c7339a22
santhusurya <santhusurya@gmail.com>
2021-08-26 01:16:33 +0530

    getFutureEventsWithWorkshops function

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    /**
+     * Get all the workshops for the event
+     */
+    public function workshop()
+    {
+        return $this->hasMany(Workshop::class,'event_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('/warmupevents', [EventsController::class, 'getEventsWithWorkshops']);
 Route::get('/events', [EventsController::class, 'getEventsWithWorkshops']);
 Route::get('/futureevents', [EventsController::class, 'getFutureEventsWithWorkshops']);
 Route::get('/menu', [MenuController::class, 'getMenuItems']);
